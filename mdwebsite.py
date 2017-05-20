0722e8fd77f1f07f7841d38f7dafd2f660ed3a2b
Dan K <teeheehee@gmail.com>
2017-05-20 11:46:46 -0400

    Tweaks to help text; check arguments.

--- mdwebsite.py
+++ mdwebsite.py
@@ -9,15 +9,28 @@
 #	Version:
 #		Refer to VERSION.md
 
+import sys
 import logging
 
 def printHelp():
 	print """\
+
 Usage: mdwebsite [OPTIONS]
+
     -h        Display this usage message
     build     Build the static website to a local output directory
     deploy    Archive existing deployed website, deploy latest build
 """
+	sys.exit()
 
 if __name__ == "__main__":
-    printHelp()
+
+	if len(sys.argv) != 2:
+		printHelp()
+
+	if sys.argv[1] == 'build':
+		print "Build!\n"
+	elif sys.argv[1] == 'deploy':
+		print "Deploy!\n"
+	else:
+		printHelp()
